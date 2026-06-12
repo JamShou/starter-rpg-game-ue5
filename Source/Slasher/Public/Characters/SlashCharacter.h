2bbafe198f455f93f27da6a91eedb83d8a055c88
JamShou <jamalshoubakics@gmail.com>
2026-06-11 23:52:34 -0500

    class cleanup

--- Source/Slasher/Public/Characters/SlashCharacter.h
+++ Source/Slasher/Public/Characters/SlashCharacter.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -12,18 +11,11 @@ class SLASHER_API ASlashCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	ASlashCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+protected:
+	virtual void BeginPlay() override;
 };
