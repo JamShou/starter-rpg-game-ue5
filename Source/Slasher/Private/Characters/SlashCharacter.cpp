3b930e92ca71ca295ad3867ae3e858619c5caa86
JamShou <jamalshoubakics@gmail.com>
2026-08-10 23:59:27 -0500

    Added Sheathe/Unsheathe logic, and Animation Montages

--- Source/Slasher/Private/Characters/SlashCharacter.cpp
+++ Source/Slasher/Private/Characters/SlashCharacter.cpp
@@ -45,7 +45,7 @@ void ASlashCharacter::BeginPlay()
 void ASlashCharacter::MoveForward(float Value)
 {
 	if (ActionState == EActionState::EAS_Attacking) return;
-	
+
 	if (Controller && (Value != 0.f))
 	{
 		StopDanceMontage();
@@ -92,6 +92,21 @@ void ASlashCharacter::EKeyPressed()
 	{
 		OverlappingWeapon->Equip(GetMesh(), FName("RightHandSocket"));
 		CharacterState = ECharacterState::ECS_EquippedOneHandedWeapon;
+		OverlappingItem = nullptr;
+		EquippedWeapon = OverlappingWeapon;
+	}
+	else
+	{
+		if (CanDisarm())
+		{
+			PlayEquipMontage(FName("Unequip"));
+			CharacterState = ECharacterState::ECS_Unequipped;
+		}
+		else if (CanArm())
+		{
+			PlayEquipMontage(FName("Equip"));
+			CharacterState = ECharacterState::ECS_EquippedOneHandedWeapon;
+		}
 	}
 }
 
@@ -119,6 +134,16 @@ void ASlashCharacter::PlayAttackMontage()
 		// }
 		//
 		// AnimInstance->Montage_JumpToSection(SectionName,AttackMontage);
+	}
+}
+
+void ASlashCharacter::PlayEquipMontage(FName SectionName)
+{
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+	if (AnimInstance && EquipMontage)
+	{
+		AnimInstance->Montage_Play(EquipMontage);
+		AnimInstance->Montage_JumpToSection(SectionName, EquipMontage);
 	}
 }
 
@@ -162,6 +187,20 @@ bool ASlashCharacter::CanAttack()
 	return ActionState == EActionState::EAS_Unoccupied &&
 		CharacterState != ECharacterState::ECS_Unequipped;
 }
+
+bool ASlashCharacter::CanDisarm()
+{
+	return ActionState == EActionState::EAS_Unoccupied &&
+		CharacterState != ECharacterState::ECS_Unequipped;
+}
+
+bool ASlashCharacter::CanArm()
+{
+	return ActionState == EActionState::EAS_Unoccupied &&
+		CharacterState == ECharacterState::ECS_Unequipped &&
+			EquippedWeapon;
+}
+
 
 void ASlashCharacter::Tick(float DeltaTime)
 {
--- Source/Slasher/Public/Characters/SlashCharacter.h
+++ Source/Slasher/Public/Characters/SlashCharacter.h
@@ -11,6 +11,7 @@ class USpringArmComponent;
 class UGroomComponent;
 class AItem;
 class UAnimMontage;
+class AWeapon;
 
 UCLASS()
 class SLASHER_API ASlashCharacter : public ACharacter
@@ -47,6 +48,10 @@ protected:
 	void AttackEnd();
 	bool CanAttack();
 	
+	void PlayEquipMontage(FName SectionName);
+	bool CanDisarm();
+	bool CanArm();
+	
 private:
 	
 	ECharacterState CharacterState = ECharacterState::ECS_Unequipped;
@@ -69,12 +74,18 @@ private:
 	UPROPERTY(VisibleInstanceOnly)
 	AItem* OverlappingItem;
 	
+	UPROPERTY(VisibleAnywhere, Category = Weapon)
+	AWeapon* EquippedWeapon;
+	
 	/**
 	 *Animation Montages
 	 */
 	UPROPERTY(EditDefaultsOnly, Category = Montages)
 	UAnimMontage* AttackMontage;
 
+	UPROPERTY(EditDefaultsOnly, Category = Montages)
+	UAnimMontage* EquipMontage;
+	
 	UPROPERTY(EditDefaultsOnly, Category = Montages)
 	UAnimMontage* DanceMontage;
 	
